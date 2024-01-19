9dc0ee2902d6e140b3e21a5d164af9c0c8ad84bd
ArmorBoyd <boydpoblete@gmail.com>
2024-01-20 01:46:21 +0800

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,11 @@
-
+// Get the current year
 const currentYear = new Date().getFullYear();
 
-
+// Set the copyright year
 document.getElementById('currentYear').textContent = currentYear;
 
-
+// Get the last modified date
 const lastModifiedDate = document.lastModified;
 
-
+// Set the last modified date in the second paragraph of the footer
 document.getElementById('lastModified').textContent = 'Last Modified: ' + lastModifiedDate;

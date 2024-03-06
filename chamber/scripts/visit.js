65a70d38e1b5e065c4602f7e264372fc9d6d00d5
ArmorBoyd <boydpoblete@gmail.com>
2024-03-06 17:14:28 +0800

    visit

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -32,11 +32,11 @@ todayDisplay.textContent = currentDate.toLocaleDateString('en-US', options);
 // Display message based on the difference in days
 if (isNaN(daysDiff) || daysDiff <= 0) {
     // First visit or same day visit
-    visitsDisplay.textContent = "Welcome! Let us know if you have any questions.";
+    sidebarContent.textContent = "Welcome! Let us know if you have any questions.";
 } else if (daysDiff === 1) {
     // Visit within 1 day
-    visitsDisplay.textContent = "Back so soon! Awesome!";
+    sidebarContent.textContent = "Back so soon! Awesome!";
 } else {
     // Visit more than 1 day ago
-    visitsDisplay.textContent = `You last visited ${daysDiff} ${daysDiff === 1 ? 'day' : 'days'} ago.`;
+    sidebarContent.textContent = `You last visited ${daysDiff} ${daysDiff === 1 ? 'day' : 'days'} ago.`;
 }

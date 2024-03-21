7b877ca69ee1b36b3f82e50a4885e3efa26f1c68
ArmorBoyd <boydpoblete@gmail.com>
2024-03-21 20:46:17 +0800

    w9

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -14,22 +14,19 @@ const displayProphets = (prophets) => {
         let card = document.createElement('section');
         let fullName = document.createElement('h2');
         let portrait = document.createElement('img');
-        let birthdate = document.createElement('p'); 
-        let birthplace = document.createElement('p'); 
+ 
         // Build the h2 content out to show the prophet's full name
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-        birthdate.textContent = `Birthdate: ${prophet.birthdate}`; 
-        birthplace.textContent = `Birthplace: ${prophet.birthplace}`;
+
         // Build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
+        
         // Append the section(card) with the created elements
         card.appendChild(fullName);
-        card.appendChild(birthdate);
-        card.appendChild(birthplace);
         card.appendChild(portrait);
         cards.appendChild(card);
     }); // end of arrow function and forEach loop

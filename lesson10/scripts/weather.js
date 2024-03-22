b2951192c860606ebaa10159577e340d28fde895
ArmorBoyd <boydpoblete@gmail.com>
2024-03-22 22:53:11 +0800

    weather.html

---
+++ lesson10/scripts/weather.js
@@ -0,0 +1,34 @@
+// Select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=60f21ce893739f0dee13f1ab33152b6d'; // Changed units to metric
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // Testing only
+            displayResults(data); // Uncomment when ready
+        } else {
+            throw new Error('Failed to fetch data');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${Math.round(data.main.temp)}&deg;C`;
+
+    const iconsrc = `https://openweathermap.org/img/wn/10d@2x.png`;
+    const desc = data.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc.charAt(0).toUpperCase() + desc.slice(1);
+}

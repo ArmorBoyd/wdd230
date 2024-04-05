124e03623112dffdf61997fe950714bbda3c68df
ArmorBoyd <boydpoblete@gmail.com>
2024-04-05 22:41:03 +0800

    rental

---
+++ AFinalProject/scripts/rentals.js
@@ -0,0 +1,34 @@
+const rentalsSection = document.querySelector('.rentals');
+
+fetch('data/scoots.json') // replace with your actual path if needed
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(rental => {
+      const rentalItem = document.createElement('div');
+      rentalItem.classList.add('rental-item');
+
+      rentalItem.innerHTML = `
+        <img src="images/${rental.image}" alt="${rental.rentalType}">
+        <h3>${rental.rentalType}</h3>
+        <table>
+          <tr>
+            <th>Rate</th>
+            <th>Half Day</th>
+            <th>Full Day</th>
+          </tr>
+          <tr>
+            <th>Reservation</th>
+            <td>$${rental.reservation.halfDay}</td>
+            <td>$${rental.reservation.fullDay}</td>
+          </tr>
+          <tr>
+            <th>Walk-In</th>
+            <td>$${rental.walkIn.halfDay}</td>
+            <td>$${rental.walkIn.fullDay}</td>
+          </tr>
+        </table>
+      `;
+
+      rentalsSection.appendChild(rentalItem);
+    });
+  });
